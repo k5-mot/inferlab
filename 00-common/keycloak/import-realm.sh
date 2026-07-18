dd0a478972c9d40f57ef4fe7c9947ebaa4f6e6ba
k5-mot <34744243+k5-mot@users.noreply.github.com>
2026-07-19 03:10:43 +0900

    🐛 fix(auth): Zulip起動とBookStack OIDCを修正

--- 00-common/keycloak/import-realm.sh
+++ 00-common/keycloak/import-realm.sh
@@ -5,6 +5,7 @@ realm_name="${STACK_NAME:-inferlab}"
 nextcloud_oidc_client_secret="${NEXTCLOUD_OIDC_CLIENT_SECRET:-sk-nextcloud-oidc-client-secret-key}"
 langfuse_oidc_client_secret="${LANGFUSE_OIDC_CLIENT_SECRET:-sk-langfuse-oidc-client-secret-key}"
 leantime_oidc_client_secret="${LEANTIME_OIDC_CLIENT_SECRET:-sk-leantime-oidc-client-secret-key}"
+bookstack_oidc_client_secret="${BOOKSTACK_OIDC_CLIENT_SECRET:-sk-bookstack-oidc-client-secret-key}"
 
 mkdir -p /opt/keycloak/data/import
 sed \
@@ -13,6 +14,7 @@ sed \
   -e "s|\${NEXTCLOUD_OIDC_CLIENT_SECRET:-sk-nextcloud-oidc-client-secret-key}|${nextcloud_oidc_client_secret}|g" \
   -e "s|\${LANGFUSE_OIDC_CLIENT_SECRET:-sk-langfuse-oidc-client-secret-key}|${langfuse_oidc_client_secret}|g" \
   -e "s|\${LEANTIME_OIDC_CLIENT_SECRET:-sk-leantime-oidc-client-secret-key}|${leantime_oidc_client_secret}|g" \
+  -e "s|\${BOOKSTACK_OIDC_CLIENT_SECRET:-sk-bookstack-oidc-client-secret-key}|${bookstack_oidc_client_secret}|g" \
   /opt/keycloak/data/import-template/inferlab-realm.json \
   > "/opt/keycloak/data/import/${realm_name}-realm.json"
 
