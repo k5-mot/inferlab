e1f64d9ab329e2897b8506965ea73bc98ab05fd3
k5-mot <34744243+k5-mot@users.noreply.github.com>
2026-07-18 17:05:34 +0900

    🐛 fix(nextcloud): 起動フックの二重suを修正

--- 12-storage/nextcloud-hooks/before-starting/10-disable-external-checks.sh
+++ 12-storage/nextcloud-hooks/before-starting/10-disable-external-checks.sh
@@ -5,7 +5,7 @@ if [ ! -f /var/www/html/config/config.php ]; then
   exit 0
 fi
 
-su -s /bin/sh www-data -c "php /var/www/html/occ config:system:set updatechecker --type boolean --value false"
-su -s /bin/sh www-data -c "php /var/www/html/occ config:system:set has_internet_connection --type boolean --value false"
-su -s /bin/sh www-data -c "php /var/www/html/occ app:disable survey_client" || true
-su -s /bin/sh www-data -c "php /var/www/html/occ app:disable recommendations" || true
+php /var/www/html/occ config:system:set updatechecker --type boolean --value false
+php /var/www/html/occ config:system:set has_internet_connection --type boolean --value false
+php /var/www/html/occ app:disable survey_client || true
+php /var/www/html/occ app:disable recommendations || true
