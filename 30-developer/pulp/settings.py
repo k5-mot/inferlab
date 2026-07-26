3335a6af4614f1727ba2d6de875e980b8763537e
k5-mot <34744243+k5-mot@users.noreply.github.com>
2026-07-26 15:13:16 +0900

    🐛 fix(developer): Pulp起動設定を修正

--- 30-developer/pulp/settings.py
+++ 30-developer/pulp/settings.py
@@ -70,14 +70,19 @@ AUTHENTICATION_BACKENDS = [
 
 TEMPLATES = [
     {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
                 "social_django.context_processors.backends",
                 "social_django.context_processors.login_redirect",
-                "dynaconf_merge",
             ],
         },
-        "dynaconf_merge": True,
     }
 ]
 
